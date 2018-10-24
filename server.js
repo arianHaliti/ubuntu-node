c48e71c965b5e2ff1436bf301cabad063cea5b0e
Arian Haliti <arian93haliti@gmail.com>
2018-10-24 10:36:18 +0200

    Added IP

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const exect = require("child_process").exec;
-
+const ip = require("ip");
 const app = express();
 
 app.get("/", (req, res) => {
@@ -12,13 +12,12 @@ app.get("/", (req, res) => {
 });
 
 app.get("/logs", (req, res) => {
-  
   exect("./../test/hello_w.sh", (err, stdout, stderr) => {
     if (err) return console.log(err);
     console.log(stdout);
     res.send(stdout);
+    console.log(ip.address());
   });
-
 });
-app.listen(8080,"192.168.1.140");
+app.listen(8080, "192.168.1.140");
 console.log("Hello gajs listening to port 8080");
