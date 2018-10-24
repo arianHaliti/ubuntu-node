3aa6aade3713bdcb5b0773739429592113981894
Arian Haliti <arian93haliti@gmail.com>
2018-10-24 10:03:09 +0200

    Added hbs/index

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
-const express = require('express');
+const express = require("express");
+const exect = require("child_process").exec;
 
 const app = express();
 
-app.get('/', (req,res) => res.send('Hello !'))
+app.get("/", (req, res) => {
+  res.render("index.hbs", {
+    pageTitle: "Home page",
 
-app.listen(8080,"192.168.1.140")
+    welcome: "Welcome to home page"
+  });
+});
+
+app.get("/logs", (req, res) => {
+  let logs = "";
+  exect("./test/hello_w.sh", (err, stdout, stderr) => {
+    if (err) throw err;
+
+    console.log(stdout);
+    logs = stdout;
+  });
+  res.send(`LOGS \n${logs}`);
+});
+app.listen(8080);
 console.log("Hello gajs listening to port 8080");
