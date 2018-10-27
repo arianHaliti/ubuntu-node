ca4036604037f2fff2f7a4b501b81e4fe1b48cbe
arian <arian93haliti@gmail.com>
2018-10-27 21:22:21 +0000

    Add check route

--- server.js
+++ server.js
@@ -19,21 +19,37 @@ app.get("/logs", (req, res) => {
   let inter = req.query.inter;
 
   exect(
-    `./../test/hello_w.sh ${log} ${path} ${inter} >> /home/arian/testOut&`,
+    `./../test/hello_w.sh ${log} ${inter} >> ${path}&`,
     (err, stdout, stderr) => {
       if (err) return console.log(err);
-      console.log(stdout);
+      
       res.send(stdout);
     }
   );
 });
 
-// app.get("/stop",(req,res)=>{
-//   exect("",(err,stdout,stderr)=>{
-//     if(err) return console.log(err);
+ app.get("/stop",(req,res)=>{
+   exect("ps -aux | grep [h]ello | awk '{print $2}'",(err,stdout,stderr)=>{
+     if(err) return console.log(err);
+     
+     let stdout_id = stdout.replace('\n',' ');
+     
+      exect("kill -9 "+stdout_id,(err,stdout,stderr)=>{
+        if(err) return console.log(err);
+          
+        })
+     let ids = stdout.trim().split("\n");
 
-//   })
-// })
+     res.send(ids);
+   })
+ })
+ app.get("/check" ,(req,res)=>{
+   exect("ps -aux | grep [h]ello | awk '{print $2}'",(err,stdout,stderr)=>{
+    //console.log(stdout.trim());
+    // let data = ""+stdout;
+     res.send(stdout.trim());
+    })
+  })
 app.listen(port, () => {
   console.log(`Listening to ${ip_address}:${port}`);
 });
